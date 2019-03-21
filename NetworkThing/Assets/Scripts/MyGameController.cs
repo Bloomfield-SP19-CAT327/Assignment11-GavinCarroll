b9dd402b877bb6edb1df2dc54183ec01e1b1b348
Gavin Carroll <gavcarroll98@gmail.com>
2019-03-21 19:14:29 -0400

    Step 6

---
+++ NetworkThing/Assets/Scripts/MyGameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyGameController : NetworkBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnEnemyTime = 0;
+
+    void Update()
+    {
+        // We only want the server to handle this...
+        if (isServer)
+        {
+            if (Time.fixedTime > spawnEnemyTime)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    [Server]
+    public void SpawnEnemy()
+    {
+        Vector3 position = new Vector3(Random.Range(-6.75f, 6.75f), Random.Range(1.0f, 8.0f), 4.5f);
+        GameObject enemy = (GameObject)Instantiate(enemyPrefab, position, Quaternion.identity);
+        NetworkServer.Spawn(enemy);
+        spawnEnemyTime = Time.fixedTime + Random.Range(3, 8);
+    }
+}
