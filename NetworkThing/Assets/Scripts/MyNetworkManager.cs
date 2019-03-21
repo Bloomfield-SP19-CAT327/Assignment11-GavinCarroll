06b478bda3b6576cf9afd6effdf80c79cdb2f21d
Gavin Carroll <gavcarroll98@gmail.com>
2019-03-21 18:50:42 -0400

    Mid Step 4

---
+++ NetworkThing/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        playerToSpawn.GetComponent<Player>().color = new Color(Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        NetworkServer.AddPlayerForConnection(conn, playerToSpawn, playerControllerId);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NetworkThing/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Player : NetworkBehaviour
+{
+    float moveSpeed = 1.875f;
+
+    [SyncVar]
+    public Color color;
+
+    public override void OnStartClient()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+    void Update()
+    {
+        GetInput();
+    }
+    void GetInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        MoveIt(x, y);
+    }
+    void MoveIt(float x, float y)
+    {
+        transform.Translate(x, y, 0);
+    }
+}
